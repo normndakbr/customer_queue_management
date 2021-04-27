5f82bdcd1881bcd99e9b4fec7b5ef8ebd480c06b
normndakbr <normnd.akbr2@gmail.com>
2021-04-27 20:53:02 +0700

    added - mongoConfig.js

--- server/app.js
+++ server/app.js
@@ -1,2 +1,13 @@
 const express = require("express")
 const app = express()
+const PORT = 3000
+const mongoose = require('mongoose')
+const { MONGO_URI } = require("./mongoConfig")
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log("Database Connected"))
+    .catch(err => console.log(err))
+
+app.listen(PORT, ()=> {
+    console.log(`http://localhost:${PORT}`);
+})
---
+++ server/mongoConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: `mongodb://electrode:voltorB95@cluster0-shard-00-00.8ptpk.mongodb.net:27017,cluster0-shard-00-01.8ptpk.mongodb.net:27017,cluster0-shard-00-02.8ptpk.mongodb.net:27017/customer_queue?ssl=true&replicaSet=atlas-40m415-shard-0&authSource=admin&retryWrites=true&w=majority`
+}
