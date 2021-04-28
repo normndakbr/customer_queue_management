c877cf6bd3d76058a11464edd5115264a435fb12
normndakbr <normnd.akbr2@gmail.com>
2021-04-28 12:24:05 +0700

    added - get all queue and get queue by id

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -3,14 +3,33 @@ const Queue = require('../models/Queue')
 class customerController {
     static async newQueue(request, response) {
         const newQueue = new Queue(request.body)
-
         try {
             const queue = await newQueue.save()
             if(!queue) throw Error('Something wrong while saving the queue')
+            response.status(201).json(queue)
+            console.log("newQueue added!")
+        } catch(err) {
+            response.status(400).json({ msg: err})
+        }
+    }
 
+    static async getAllQueue(request, response) {
+        try {
+            const queue = await Queue.find()
+            if(!queue) throw Error('No Items')
             response.status(200).json(queue)
         } catch(err) {
             response.status(400).json({ msg: err})
+        }
+    }
+
+    static async getQueueById(request, response) {
+        try {
+            const queue = await Queue.findById(request.params.id)
+            if(!queue) throw Error('Item not found')
+            response.status(200).json(queue)
+        } catch(err) {
+            response.status(400).json({ msg: err })
         }
     }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 const customerController = require('../controllers/customerController')
 
 router.post('/newQueue', customerController.newQueue)
+router.get('/getAllQueue', customerController.getAllQueue)
+router.get('/getQueueById/:id', customerController.getQueueById)
 
 module.exports = router
