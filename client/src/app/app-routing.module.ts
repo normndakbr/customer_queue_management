9c44690fc3e1fb44401ffb8664218f2f3200a3f9
normndakbr <normnd.akbr2@gmail.com>
2021-05-01 09:14:36 +0700

    set up client routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputVisitorDetailsComponent } from './components/input-visitor-details/input-visitor-details.component';
+import { VisitorListComponent } from './components/visitor-list/visitor-list.component';
+import { RequestQueueNumberComponent } from './components/request-queue-number/request-queue-number.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'input-visitor', component: InputVisitorDetailsComponent },
+  { path: 'request-number/:id', component: RequestQueueNumberComponent },
+  { path: 'list-visitor', component: VisitorListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
