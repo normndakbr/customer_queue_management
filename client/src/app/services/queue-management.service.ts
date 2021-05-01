dc7fc499e5b04d34327ddf6573902870eb0446cb
normndakbr <normnd.akbr2@gmail.com>
2021-05-01 10:32:45 +0700

    setup app.component.html

--- client/src/app/services/queue-management.service.ts
+++ client/src/app/services/queue-management.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const url = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QueueManagementService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
 }
